e0c9fd371dcc86f85012245d79dee8797d806533
Arun Karnati <arun.karnati@gmail.com>
2017-08-15 16:12:46 -0700

    test for permission voter

--- tests/Security/PbjxPermissionVoterTest.php
+++ tests/Security/PbjxPermissionVoterTest.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 namespace Gdbots\Tests\Bundle\IamBundle;
 
 use Gdbots\Bundle\IamBundle\Security\PbjxPermissionVoter;
+use Gdbots\Pbjx\Pbjx;
+use Gdbots\Pbjx\EventStore\InMemoryEventStore;
+use Gdbots\Pbjx\RegisteringServiceLocator;
+use Gdbots\Ncr\Repository\InMemoryNcr;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
 
 class PbjxPermissionVoterTest extends TestCase
 {
@@ -20,6 +27,12 @@ class PbjxPermissionVoterTest extends TestCase
     /** @var InMemoryNcr */
     protected $ncr;
 
+    /** @var  PbjxPermissionVoter */
+    protected $pbjxPermissionVoter;
+
+    /** @var  AccessDecisionManagerInterface */
+    protected $decisionManager;
+
     protected function setup()
     {
         $this->locator = new RegisteringServiceLocator();
@@ -31,6 +44,21 @@ class PbjxPermissionVoterTest extends TestCase
 
     public function testVote()
     {
-        $voter = new PbjxPermissionVoter();
+        $firewall = 'secured_area';
+        $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
+        $subject = '';
+        $attributes = ['acme:blog:command:create-article'];
+        $expected = true;
+
+        $this->decisionManager = new class implements AccessDecisionManagerInterface
+        {
+            public function decide(TokenInterface $token, array $attributes, $object = null)
+            {
+                return true;
+            }
+        };
+
+        $voter = new PbjxPermissionVoter($this->decisionManager, $this->pbjx);
+        $this->assertEquals($expected, $voter->vote($token, $subject, $attributes));
     }
 }
